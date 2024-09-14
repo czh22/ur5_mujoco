9686a97c0f104e5c2c0b9492f03865c2c46e2eb3
czh22 <2152223@tongji.edu.cn>
2024-09-14 15:16:00 +0800

    update pure_mujoco.py

--- pure_mujoco.py
+++ pure_mujoco.py
@@ -2,44 +2,42 @@ import mujoco
 import mujoco.viewer
 import time
 
-import camera2pos
 import numpy as np
 from scipy.spatial.transform import Rotation
-import math
 import admittance_py
 from ur_ikfast import ur_kinematics
-import imu_receiver
+
+
 
 # 设置admittance控制参数
-M = np.array([[5, 0, 0, 0, 0, 0],
-     [0, 5, 0, 0, 0, 0],
-     [0, 0, 5, 0, 0, 0],
-     [0, 0, 0, 5, 0, 0],
-     [0, 0, 0, 0, 5, 0],
-     [0, 0, 0, 0, 0, 5]])
+M = np.array([[10, 0, 0, 0, 0, 0],
+     [0, 10, 0, 0, 0, 0],
+     [0, 0, 10, 0, 0, 0],
+     [0, 0, 0, 10, 0, 0],
+     [0, 0, 0, 0, 10, 0],
+     [0, 0, 0, 0, 0, 10]])
 B = np.array([[5, 0, 0, 0, 0, 0],
      [0, 5, 0, 0, 0, 0],
      [0, 0, 5, 0, 0, 0],
      [0, 0, 0, 5, 0, 0],
      [0, 0, 0, 0, 5, 0],
      [0, 0, 0, 0, 0, 5]])
-K = np.array([[10, 0, 0, 0, 0, 0],
-     [0, 10, 0, 0, 0, 0],
-     [0, 0, 10, 0, 0, 0],
-     [0, 0, 0, 10, 0, 0],
-     [0, 0, 0, 0, 10, 0],
-     [0, 0, 0, 0, 0, 10]])
+K = np.array([[20, 0, 0, 0, 0, 0],
+     [0, 20, 0, 0, 0, 0],
+     [0, 0, 20, 0, 0, 0],
+     [0, 0, 0, 20, 0, 0],
+     [0, 0, 0, 0, 20, 0],
+     [0, 0, 0, 0, 0, 20]])
 current_vel = np.array([0, 0, 0, 0, 0, 0])
 current_pos = np.array([0, 0, 0, 0, 0, 0])
 
 
 # 初始化ur5逆解
 ur5e_arm = ur_kinematics.URKinematics('ur5e')
-# ur5_module = ur5_ik.ur5_ik()
 
-# model = mujoco.MjModel.from_xml_path('universal_robots_ur5e/ur5e.xml')
-model = mujoco.MjModel.from_xml_path('demo.xml')
-# model.opt.gravity = 0
+model = mujoco.MjModel.from_xml_path('universal_robots_ur5e/ur5e.xml')
+# model = mujoco.MjModel.from_xml_path('demo.xml')
+model.opt.gravity = 0
 data = mujoco.MjData(model)
 # 横着
 # rotation = np.array([[-1, 0, 0],
@@ -53,9 +51,12 @@ rotation = np.array([[-1.64292755e-03,  9.98002831e-01, -6.31478444e-02],
 
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
+    real_start_time = time.time()
     while viewer.is_running():
+        
+        
         position = [-0.75, 0.1, 0.2]
-        start_time = time.time()
+        
 
 
         if position is not None:
@@ -85,7 +86,7 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
                 torque = data.sensor('torque_sensor').data
                 end_force = np.concatenate((force, torque), axis=0)
                 # end_force = np.array([0, 0, 0, 0, 0, 0])
-                # print(end_force)
+                print(end_force)
 # 计算admittance控制量
                 current_pos, current_vel = admittance_py.admittance_pos(K, B, M, current_vel, current_pos, end_force, target_pos)
 
@@ -94,13 +95,16 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
                 target_pos = np.concatenate((matrix, current_pos[:3, np.newaxis]), axis=1)
                 # print(target_pos)
                 # print(data.qpos.shape)
-                # ctrl = ur5e_arm.inverse(target_pos, False, data.qpos[-6:])
+                ctrl = ur5e_arm.inverse(target_pos, False, data.qpos[-6:])
                 # print(ctrl)
 # 设置控制量
                 if ctrl is not None:
                     print(True)
                     data.ctrl[-6:] = ctrl
 # 进行仿真
+        current_time = time.time()
+        if current_time - real_start_time < data.time:
+            time.sleep(data.time - (current_time - real_start_time))
         mujoco.mj_step(model, data, 1)
         viewer.sync()
 
